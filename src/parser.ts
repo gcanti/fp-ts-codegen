a558a8269c3657a858f3806e9e05ad2503200aa8
gcanti <giulio.canti@gmail.com>
2019-01-22 13:41:44 +0100

    Internal: upgrade to latest parser-ts

--- src/parser.ts
+++ src/parser.ts
@@ -12,17 +12,15 @@ const identifierFirstLetter = P.sat(c => !isDigit(c) && !isPunctuation(c))
 
 const identifierBody = P.sat(c => !isPunctuation(c))
 
-export const identifier: P.Parser<string> = P.expected(
+export const identifier: P.Parser<string> = P.expectedL(
   P.fold([identifierFirstLetter, C.many(identifierBody)]),
-  'an identifier'
+  s => `Expected an identifier, got ${JSON.stringify(s)}`
 )
 
 const typeWithoutParens: P.Parser<M.Type> = identifier.map(name => M.type(name, []))
 
-const typeWithParens = C.char('(').chain(() =>
-  identifier
-    .chain(name => S.spaces.chain(() => P.many(type).map(types => M.type(name, types))))
-    .chain(type => C.char(')').map(() => type))
+const typeWithParens = C.char('(').applySecond(
+  identifier.chain(name => S.spaces.applySecond(P.many(type).map(types => M.type(name, types)))).applyFirst(C.char(')'))
 )
 
 export const type: P.Parser<M.Type> = typeWithParens.alt(typeWithoutParens)
@@ -30,7 +28,7 @@ export const type: P.Parser<M.Type> = typeWithParens.alt(typeWithoutParens)
 export const positionalMember: P.Parser<M.Member> = type.map(M.positionalMember)
 
 export const namedMember: P.Parser<M.Member> = identifier.chain(name =>
-  C.char(':').chain(() => type.map(type => M.namedMember(name, type)))
+  C.char(':').applySecond(type.map(type => M.namedMember(name, type)))
 )
 
 export const member: P.Parser<M.Member> = namedMember.alt(positionalMember)
@@ -38,27 +36,27 @@ export const member: P.Parser<M.Member> = namedMember.alt(positionalMember)
 export const members: P.Parser<Array<M.Member>> = P.sepBy(S.spaces, member)
 
 export const constructor: P.Parser<M.Constructor> = identifier.chain(name =>
-  S.spaces.chain(() => members.map(members => M.constructor(name, members)))
+  S.spaces.applySecond(members.map(members => M.constructor(name, members)))
 )
 
 const equal = S.spaces.chain(() => C.char('='))
 
 export const introduction: P.Parser<M.Introduction> = S.string('data').chain(() =>
-  S.spaces.chain(() =>
+  S.spaces.applySecond(
     identifier.chain(name =>
       S.spaces
-        .chain(() => P.sepBy(S.spaces, identifier))
+        .applySecond(P.sepBy(S.spaces, identifier))
         .chain(parameters => equal.map(() => M.introduction(name, parameters)))
     )
   )
 )
 
-const pipe = S.spaces.chain(() => C.char('|')).chain(() => S.spaces)
+const pipe = P.fold([S.spaces, C.char('|'), S.spaces])
 
 export const data: P.Parser<M.Data> = introduction.chain(definition =>
-  S.spaces.chain(() => P.sepBy1(pipe, constructor).map(constructors => M.data(definition, constructors)))
+  S.spaces.applySecond(P.sepBy1(pipe, constructor).map(constructors => M.data(definition, constructors)))
 )
 
 export const parse = (s: string): Either<string, M.Data> => {
-  return data.run(s).bimap(e => `Expected ${e.message}, got ${JSON.stringify(e.remaining)}`, ([data]) => data)
+  return data.run(s).bimap(e => e.message, ([data]) => data)
 }
